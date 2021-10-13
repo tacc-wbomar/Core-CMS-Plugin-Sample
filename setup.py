e786c0efcca80be4db96768c0cdaf8f7157a7478
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-10-13 16:19:35 -0500

    Quick: Fix wrong repo URL
    - It was TACC so I would not need to edit this line in the future.
    - But the repo will be renamed also. Must edit later anyway, so fix.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='BSD License',
     description='A sample DjangoCMS plugin (for TACC Core CMS) to greet a user.',
     long_description=README,
-    url='https://github.com/TACC/Core-CMS-Plugin-Sample/',
+    url='https://github.com/tacc-wbomar/Core-CMS-Plugin-Sample/',
     author='Wesley Bomar',
     author_email='wbomar@tacc.utexas.edu',
     # SEE: https://pypi.org/classifiers/
